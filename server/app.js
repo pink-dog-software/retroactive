a494b5b44aaf949b1cb7bf18acfd21a165a6bd4e
Graham Langford <grahamlangford87@gmail.com>
2018-11-12 12:51:49 -0600

    fix: updated linting rules

--- server/app.js
+++ server/app.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
+/* eslint-disable no-console */
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+const bodyParser = require('body-parser')
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
-);
+)
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use(helmet());
+app.use(helmet())
 
-app.use(cors());
+app.use(cors())
 
 const server = app.listen(port, () => {
-  const host = server.address().address;
+  const host = server.address().address
 
-  console.log(`App listening at http://${host}:${port}`);
-});
+  console.log(`App listening at http://${host}:${port}`)
+})
 
-module.exports = server;
+module.exports = server
