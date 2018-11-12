7033f82b8a1d2429fb265a086c1b4aee1643c48f
Graham Langford <grahamlangford87@gmail.com>
2018-11-12 12:35:23 -0600

    chore: added fonts, MuiThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HelloWorld from './Components/HelloWorld/HelloWorld';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import HelloWorld from './Components/HelloWorld/HelloWorld'
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+const theme = createMuiTheme()
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <HelloWorld />
+  </MuiThemeProvider>,
+  document.getElementById('app')
+)
